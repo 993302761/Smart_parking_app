59d305b36505129d53ed527c7d9f111c6099867b
993302761 <993302761@qq.com>
2022-11-14 23:05:32 +0800

    update

--- app/src/main/java/com/example/myapplication/home/MeFragment.java
+++ app/src/main/java/com/example/myapplication/home/MeFragment.java
@@ -1,9 +1,12 @@
 package com.example.myapplication.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.example.myapplication.R;
 
 import androidx.annotation.NonNull;
@@ -12,9 +15,26 @@ import androidx.fragment.app.Fragment;
 
 public class MeFragment extends Fragment {
 
+    private Button order;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_me,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        order=(Button) view.findViewById(R.id.button3);
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), OrderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
